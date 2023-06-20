5f6c2b0713d1f545aef3057baf343e14c3b6dcbc
gustavo <gtslxp@gmail.com>
2023-06-19 22:06:53 -0300

    added fixes

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
-const hamb = document.querySelector('.hamb');
-const nav = document.querySelector('.nav');
+const hamb = document.querySelector(".hamb");
+const nav = document.querySelector(".nav");
 
-hamb.addEventListener('click', () => {
-  nav.classList.toggle('active');
-})
+hamb.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
 
-document.addEventListener('click', (e) => {
+document.addEventListener("click", (e) => {
   if (!nav.contains(e.target) && !hamb.contains(e.target)) {
-    nav.classList.remove('active');
+    nav.classList.remove("active");
   }
-})
+});
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// =-                  G O   T O                  -=
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    const target = document.querySelector(this.getAttribute("href"));
+
+    window.scrollTo({
+      top: target.offsetTop,
+      behavior: "smooth",
+    });
+  });
+});
